762c7f3009bdeecf9966e7868dcbea6348843f1e
Drew Bayles <dtbayles@gmail.com>
2024-04-27 17:25:01 +0000

    update config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: "."
 }
 
 module.exports = nextConfig
